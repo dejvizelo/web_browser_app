16298db34fa903731bc51b6414ba79237c02d1a5
Dejvi <dejvi.zelo@gmail.com>
2020-10-02 16:04:47 +0200

    Update README

--- lib/main.dart
+++ lib/main.dart
@@ -24,82 +24,61 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('InAppWebView Example'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                if (_webViewController != null) {
+                  _webViewController.goBack();
+                }
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                if (_webViewController != null) {
+                  _webViewController.goForward();
+                }
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                if (_webViewController != null) {
+                  _webViewController.reload();
+                }
+              },
+            )
+          ],
         ),
         body: Container(
-            child: Column(children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Text(
-                    "CURRENT URL\n${(url.length > 50) ? url.substring(0, 50) + "..." : url}"),
+            child: InAppWebView(
+              initialUrl: "https://flutter.dev/",
+              initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions(
+                    debuggingEnabled: true,
+                  )
               ),
-              Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: progress < 1.0
-                      ? LinearProgressIndicator(value: progress)
-                      : Container()),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  decoration:
-                  BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-                  child: InAppWebView(
-                    initialUrl: "https://flutter.dev/",
-                    initialOptions: InAppWebViewGroupOptions(
-                        crossPlatform: InAppWebViewOptions(
-                          debuggingEnabled: true,
-                        )
-                    ),
-                    onWebViewCreated: (InAppWebViewController controller) {
-                      _webViewController = controller;
-                    },
-                    onLoadStart: (InAppWebViewController controller, String url) {
-                      setState(() {
-                        this.url = url;
-                      });
-                    },
-                    onLoadStop: (InAppWebViewController controller, String url) async {
-                      setState(() {
-                        this.url = url;
-                      });
-                    },
-                    onProgressChanged: (InAppWebViewController controller, int progress) {
-                      setState(() {
-                        this.progress = progress / 100;
-                      });
-                    },
-                  ),
-                ),
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    child: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      if (_webViewController != null) {
-                        _webViewController.goBack();
-                      }
-                    },
-                  ),
-                  RaisedButton(
-                    child: Icon(Icons.arrow_forward),
-                    onPressed: () {
-                      if (_webViewController != null) {
-                        _webViewController.goForward();
-                      }
-                    },
-                  ),
-                  RaisedButton(
-                    child: Icon(Icons.refresh),
-                    onPressed: () {
-                      if (_webViewController != null) {
-                        _webViewController.reload();
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ])),
+              onWebViewCreated: (InAppWebViewController controller) {
+                _webViewController = controller;
+              },
+              onLoadStart: (InAppWebViewController controller, String url) {
+                setState(() {
+                  this.url = url;
+                });
+              },
+              onLoadStop: (InAppWebViewController controller, String url) async {
+                setState(() {
+                  this.url = url;
+                });
+              },
+              onProgressChanged: (InAppWebViewController controller, int progress) {
+                setState(() {
+                  this.progress = progress / 100;
+                });
+              },
+            ),
+          ),
       ),
     );
   }
